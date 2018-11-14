4fd079a0b4fc5256d5b28cf8bb86badb508d7543
Lucas Amorim <lea@cin.ufpe.br>
2018-11-14 01:21:41 -0300

    Alterações em erro que deixava ambas as classes com 0 em todos os registros

--- pre_proc_TKN.py
+++ pre_proc_TKN.py
@@ -21,10 +21,10 @@ data_set_c2 = data_set.loc[data_set.IND_BOM_1_2 == 1]
 #Classe 1
 
 previsores_c1 = data_set_c1.iloc[:,1:-2]
-classe_c1 = data_set_c1.iloc[:,-1]
+classe_c1 = data_set_c1.iloc[:,-2]
 #Classe 2
 previsores_c2 = data_set_c2.iloc[:,1:-2]
-classe_c2 = data_set_c2.iloc[:,-2]
+classe_c2 = data_set_c2.iloc[:,-1]
 
 #Separação entre Treinamento (50%), Validação(25%) e teste(25%)
 #Conjuntos de Treinamento, Validação e Teste da Classe 1
@@ -48,14 +48,14 @@ c2_val = c2_val.sample(n=c1_val.count(),random_state = 1, replace = True)
 Obtendo o conjunto de Treinamento final com as classes juntas
 '''
 #Classe 1
-zeros = pd.DataFrame(np.zeros((c1_train.count(),1)),columns = ['IND_BOM_1_1'])
-c1_train = pd.concat([zeros,c1_train.reset_index(drop = True)],axis=1,) #concatena a coluna a direita
+zeros = pd.DataFrame(np.zeros((c1_train.count(),1)),columns = ['IND_BOM_1_2'])
+c1_train = pd.concat((c1_train.reset_index(drop = True),zeros),axis=1) #concatena a coluna a direita
 #Classe 2
-zeros = pd.DataFrame(np.zeros((c2_train.count(),1)),columns = ['IND_BOM_1_2'])
-c2_train = pd.concat([c2_train.reset_index(drop = True),zeros],axis=1,) #concatena a coluna a direita
+zeros = pd.DataFrame(np.zeros((c2_train.count(),1)),columns = ['IND_BOM_1_1'])
+c2_train = pd.concat((zeros,c2_train.reset_index(drop = True)),axis=1) #concatena a coluna a direita
 
 #junta o vetor das duas classes
-c_train = c1_train.append([c2_train]).reset_index(drop=True)
+c_train = c2_train.append([c1_train]).reset_index(drop=True)
 
 #Previsores Classe 1 + Previsores Classe 2
 p_train = p_c1_train.append(p_c2_train).reset_index(drop=True)
@@ -71,11 +71,11 @@ train.to_csv('treinamento.csv') #Escrevendo num CSV os dados que serão usados p
 Validação
 '''
 #Classe 1
-zeros = pd.DataFrame(np.zeros((c1_val.count(),1)),columns = ['IND_BOM_1_1'])
-c1_val = pd.concat([zeros,c1_val.reset_index(drop = True)],axis=1,) #concatena a coluna a direita
+zeros = pd.DataFrame(np.zeros((c1_val.count(),1)),columns = ['IND_BOM_1_2'])
+c1_val = pd.concat([c1_val.reset_index(drop = True),zeros],axis=1) #concatena a coluna a direita
 #Classe 2
-zeros = pd.DataFrame(np.zeros((c2_val.count(),1)),columns = ['IND_BOM_1_2'])
-c2_val = pd.concat([c2_val.reset_index(drop = True),zeros],axis=1,) #concatena a coluna a direita
+zeros = pd.DataFrame(np.zeros((c2_val.count(),1)),columns = ['IND_BOM_1_1'])
+c2_val = pd.concat([zeros,c2_val.reset_index(drop = True)],axis=1) #concatena a coluna a direita
 
 #junta o vetor das duas classes
 c_val = c1_val.append([c2_val]).reset_index(drop=True)
@@ -92,11 +92,11 @@ val.to_csv('validacao.csv')
 '''
 Teste
 '''
-zeros = pd.DataFrame(np.zeros((c1_test.count(),1)),columns = ['IND_BOM_1_1'])
-c1_test = pd.concat([zeros,c1_test.reset_index(drop = True)],axis=1,) #concatena a coluna a direita
+zeros = pd.DataFrame(np.zeros((c1_test.count(),1)),columns = ['IND_BOM_1_2'])
+c1_test = pd.concat([zeros,c1_test.reset_index(drop = True)],axis=1) #concatena a coluna a direita
 #Classe 2
-zeros = pd.DataFrame(np.zeros((c2_test.count(),1)),columns = ['IND_BOM_1_2'])
-c2_test = pd.concat([c2_test.reset_index(drop = True),zeros],axis=1,) #concatena a coluna a direita
+zeros = pd.DataFrame(np.zeros((c2_test.count(),1)),columns = ['IND_BOM_1_1'])
+c2_test = pd.concat([c2_test.reset_index(drop = True),zeros],axis=1) #concatena a coluna a direita
 
 #junta o vetor das duas classes
 c_test = c1_test.append([c2_test]).reset_index(drop=True)
